fa9909fe59b7aa1c84bee681216447ba60ffaed3
SaraMoreno98 <saramo.graphic@gmail.com>
2024-08-09 10:46:09 +0200

    Commit

---
+++ js/parallel.js
@@ -0,0 +1,10 @@
+// IMAGEN DE FONDO
+document.getElementById('parallax').style.backgroundImage = "url('../img/alexander-psiuk-utfiDJBebHw-unsplash.jpg')"
+
+// FUNCION PARA CONTROLAR LA VELOCIDAD DEL SCROLL
+window.addEventListener('scroll', function(){
+    let parallax = document.getElementById('parallax')
+    let scrollPosition = this.window.scrollY
+    // CAMBIAR POSICION DE LA IMG EN FUNCION DEL SCROLL, EL 0.7 CAMBIA LA VELOCIDAD
+    parallax.style.backgroundPositionY = (scrollPosition * 0.7) + 'px'
+})
